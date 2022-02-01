699fdc229f6e470877c472df1c1cf72dc99cceb3
33kumarram <95571520+33kumarram@users.noreply.github.com>
2022-02-01 17:03:15 +0530

    removed some unwanted stuff

--- src/App.js
+++ src/App.js
@@ -6,25 +6,10 @@ class app extends Component {
   constructor(){
     super();
     this.state={
-      monsters:[
-        // {
-        //   name:'Photoshop',
-        //   id:'ps'
-        // },
-        // {
-        //   name:'Illustrator',
-        //   id:'ai'
-        // },
-        // {
-        //   name:'Figma',
-        //   id:'fg'
-        // },
-      ],
-      searchfield : ''
-     
-      // string:'hello'
+      monsters:[],
+      searchfield : ''  
     };
-    // this.handlechange=this.handlechange.bind(this);
+     
   }
   componentDidMount(){
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -39,19 +24,7 @@ class app extends Component {
     const FilteredMonsters = monsters.filter(monster =>monster.name.toLowerCase().includes(searchfield.toLowerCase()))
   return (
     <div className="App">
-      {/* <input type="search" placeholder='search monsters' onChange={e=>{
-        this.setState({searchfield:e.target.value},
-          ()=>console.log(this.state)
-        );
-        } }
-        />
-     <Cardlist name='yuha'>yuhaa</Cardlist>
-     <Cardlist name='yuha'>
-     { this.state.monsters.map(monster => (
-        <h1 key={monster.id}>{monster.name}</h1>
-      ))}
-       </Cardlist>   
-       <Cardlist monsters={this.state.monsters} /> */}
+    
        <h1>Monster Rolodex</h1>
        <SearchBox
        placeholder='search-monsters'
@@ -59,23 +32,7 @@ class app extends Component {
       handlechange={this.handlechange}
           />
           <Cardlist monsters={FilteredMonsters} />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.string}
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button onClick={()=>this.setState({string:'Hello mc'})}>Learn more</button>
-        </a> */}
        
-          {/* <button onClick={()=>this.setState({string:'Hello mc'})}>donotpress</button>
-      </header> */}
-    
     </div>
   );
   }
--- src/components/card-list/card-listcomponents.js
+++ src/components/card-list/card-listcomponents.js
@@ -2,13 +2,6 @@ import React from "react";
 import './card-list.style.css';
 import { Card } from "../components1/card.component";
 export const Cardlist=props=>{
-//  return(
-//  console.log(props),
-//  <div>Hello</div>,
-//  <div>{props.name}</div>,
-// <div className='card-list'>{props.children}</div>
-   
-//  )
 return(
 <div className='card-list'>
 {props.monsters.map(monster => (
