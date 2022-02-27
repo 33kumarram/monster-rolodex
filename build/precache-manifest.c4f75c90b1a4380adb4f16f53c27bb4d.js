a4b1969e93cc09a2ee4e7bcdd112c521e2c8a44a
33kumarram <95571520+33kumarram@users.noreply.github.com>
2022-02-27 20:20:15 +0530

    adding files for git hub pages

---
+++ build/precache-manifest.c4f75c90b1a4380adb4f16f53c27bb4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0feada2347b0c4893b8e1b3eb92c47d7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1cd96ddb79b68927dc41",
+    "url": "/monster-rolodex/static/css/main.bdb72961.chunk.css"
+  },
+  {
+    "revision": "d4078db62c75187ff8b4",
+    "url": "/monster-rolodex/static/js/2.f0373fab.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/monster-rolodex/static/js/2.f0373fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b58e8430db71b1ef82cf",
+    "url": "/monster-rolodex/static/js/3.26e39fa9.chunk.js"
+  },
+  {
+    "revision": "1cd96ddb79b68927dc41",
+    "url": "/monster-rolodex/static/js/main.ef0031e4.chunk.js"
+  },
+  {
+    "revision": "55cbf57e8225f3c5cc63",
+    "url": "/monster-rolodex/static/js/runtime-main.066cb4d5.js"
+  }
+]);
